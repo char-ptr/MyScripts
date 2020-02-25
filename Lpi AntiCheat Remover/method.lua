91825c982a58c642cab95acf2bc66fa3d151adb0
pozm <44528100+pozm@users.noreply.github.com>
2020-02-25 02:54:00 +0000

    Create method.lua

---
+++ Lpi AntiCheat Remover/method.lua
@@ -0,0 +1,37 @@
+function Method()
+
+	http = game.HttpService
+	desc = game:GetDescendants()
+
+	plr = game.Players.LocalPlayer
+
+	plrpg = plr:WaitForChild('PlayerGui')
+
+	d2 = plrpg:GetChildren()
+	local ac 
+
+	for _,v in pairs(d2) do 
+
+		if v:IsA('LocalScript') then 
+
+			--print(v.Name, '-B'..string.byte(v.Name),'-ut',utf8.len(v.Name),'-sl'..#v.Name)
+
+			if utf8.len(v.Name) == 4 then
+
+				ac = v
+
+			end
+
+		end
+
+	end
+
+	if game:GetService("ReplicatedStorage")[" "] then game:GetService("ReplicatedStorage")[" "]:Destroy() end
+
+	if ac then ac:Destroy() end
+
+	return true
+
+end
+
+return Method

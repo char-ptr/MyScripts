aa92fc7f75718e6ff20fd32a8ef8bfe33f7e5827
pozm <44528100+pozm@users.noreply.github.com>
2020-04-09 16:08:58 +0100

    Create acr.lua

---
+++ LPI/acr.lua
@@ -0,0 +1,9 @@
+_G.BlackListedScripts = {
+    
+    "\226\128\140\226\128\140",
+    "\226\128\140\226\128\140\226\128\140\226\128\140"
+    
+    
+}
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/pozm/MyScripts/master/General/ScriptFucker.lua'),true))()

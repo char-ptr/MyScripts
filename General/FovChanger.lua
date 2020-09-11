aa5d59ffd70f0d04a76d92377a58fff7a7de155f
brad <44528100+pozm@users.noreply.github.com>
2020-09-11 15:31:16 +0100

    Update FovChanger.lua

--- General/FovChanger.lua
+++ General/FovChanger.lua
@@ -3,6 +3,9 @@ val = 100
 cam = game.workspace:WaitForChild("Camera")
 local caml 
 local player
+-- i do this incase the user is using auto launch + auto exec,
+-- using this combination normally will break the script due to
+-- it being executed before player is defined.
 while not player do player = game.Players.LocalPlayer; wait(1) end
 
 cam.FieldOfView = val
